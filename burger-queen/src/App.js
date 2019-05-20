582f4f8815db1877a1dcc1e58fae7682f039b6f1
KarenHippitaku <hippitaku@gmail.com>
2019-05-20 13:37:04 -0500

    Rendering json data without errors

--- burger-queen/src/App.js
+++ burger-queen/src/App.js
@@ -1,26 +1,87 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {menu} from './menu.json';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      menu
+    }
+  }
+  render() {
+    // console.log(this.state.menu.dishes);
+    const dishes = this.state.menu.dishes.map((dish, i) => {
+      return (
+        <dl className="card" key={i}>
+          <dt className="cardHeader">
+          <h3>{dish.title}</h3>
+          </dt>
+          <dt className="cardBody">
+          <p>{dish.price}</p>
+          </dt>
+        </dl>
+      )
+    });
+
+    const toppings = this.state.menu.toppings.map((topping, i) => {
+      return (
+        <dl className="card" key={i}>
+          <dt className="cardHeader">
+          <h3>{topping.title}</h3>
+          </dt>
+          <dt className="cardBody">
+          <p>{topping.price}</p>
+          </dt>
+        </dl>
+      )
+    });
+
+    const drinks = this.state.menu.drinks.map((drink, i) => {
+      return (
+        <dl className="card" key={i}>
+          <dt className="cardHeader">
+          <h3>{drink.title}</h3>
+          </dt>
+          <dt className="cardBody">
+          <p>{drink.price}</p>
+          </dt>
+        </dl>
+      )
+    })
+
+    return (
+      <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+      Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a className="text-white">
+      Portable menu
+      </a>
       </header>
-    </div>
-  );
+      <section className="menuSection">
+        <div className="container dishes">
+        <div className="row mt-4">
+          {dishes}
+        </div>
+        <div className="row mt-4">
+          {toppings}
+        </div>
+        </div>
+      </section>
+      <section className="menuSection">
+        <div className="container dishes">
+        <div className="row mt-4">
+          {drinks}
+        </div>
+        </div>
+      </section>
+      </div>
+    );
+  }
 }
 
 export default App;
