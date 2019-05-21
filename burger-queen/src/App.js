6ba92e67ce37d88aa02912b98e82b112fa3ff2d4
KarenHippitaku <hippitaku@gmail.com>
2019-05-21 13:08:48 -0500

    Column size at 8. And Juan workshop on comments

--- burger-queen/src/App.js
+++ burger-queen/src/App.js
@@ -3,22 +3,54 @@ import React, {Component} from 'react';
 import './App.css';
 import Navigation from './components/Navigation';
 // import MenuButton from './components/MenuButton';
-import Card from './components/Card';
+import Order from './components/Order';
 import {menu} from './menu.json';
 
 class App extends Component {
+  //We can skip super() or even the whole constructor() and you can write directly state = {}
   constructor() {
     super();
     this.state = {
       menu
     }
   }
+  //updateExample = () => {
+    //this.setState({ object: value });
+  //}
+  //<Button name="Your botton action" action={this.updateExample} />
+
+  //state = {
+  //color: 'blue'
+  //}
+  //updateBackgroundColor = (color) => {
+    //this.setState({ color: color });
+  //}
+  //<Button name="change to red" action={() => this.updateBackgroundColor('red')} />
+  //<Button name="change to yellow" action={() => this.updateBackgroundColor('yellow')} />
+
+  //state = {
+  //color: 'blue',
+  //basicColor: 'blue'
+  //}
+  //updateBackgroundColor = (color) => {
+    //if (this.state.color === 'blue') {
+    //this.setState({ color: color });
+    //return;
+    //} else {
+    //this.setState({ color: 'blue' });
+    //}
+  //}
+  //<Button name="change to red" action={() => this.updateBackgroundColor('red')} />
+  //<Button name="change to yellow" action={() => this.updateBackgroundColor('yellow')} />
+
 
   // handleAddOrder(dish, topping, drink) {
   //   this.state({
   //     menu: [...this.state.menu, ]
   //   })
   // }
+
+// const props = this.props; //declares props in a class kind Component
 
   render() {
     // console.log(this.state.menu.dishes);
@@ -66,7 +98,7 @@ class App extends Component {
       <header className="App-header">
       <Navigation/>
       </header>
-      <section className="menuSection col-md-4">
+      <section className="menuSection col-md-8">
         <div className="container dishes">
         <div className="row mt-4">
           {dishes}
@@ -76,7 +108,7 @@ class App extends Component {
         </div>
         </div>
       </section>
-      <section className="menuSection col-md-4">
+      <section className="menuSection col-md-8">
         <div className="container dishes">
         <div className="row mt-4">
           {drinks}
@@ -84,7 +116,7 @@ class App extends Component {
         </div>
       </section>
       <section>
-      <Card/>
+      <Order/>
       </section>
       </div>
     );
