fa5db04c6a9ca6758f756849e503087d631cc91b
KarenHippitaku <hippitaku@gmail.com>
2019-05-27 13:16:58 -0500

    Created to comunnicate Order and current menu (dishes,toppings,drinks)

--- burger-queen/src/components/MenuButton.js
+++ burger-queen/src/components/MenuButton.js
@@ -1,54 +1,15 @@
 import React from 'react';
-import {menu} from '../menu.json';
 
-class MenuButton extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      menu
-    }
-  }
-  render() {
-    // console.log(this.state.menu.dishes);
-    const dishes = this.state.menu.dishes.map((dish, i) => {
+const MenuButton = (props) => {
       return (
-        <dl className="card" key={i}>
+        <dl className="card" key={props.index} onClick={props.onClick}>
           <dt className="card-header">
-          <h3>{dish.title}</h3>
+          <h3>{props.title}</h3>
           </dt>
           <dt className="card-body">
-          <p>{dish.price}</p>
+          <p>{props.price}</p>
           </dt>
         </dl>
       )
-    });
-
-    const toppings = this.state.menu.toppings.map((topping, i) => {
-      return (
-        <dl className="card" key={i}>
-          <dt className="card-header">
-          <h3>{topping.title}</h3>
-          </dt>
-          <dt className="card-body">
-          <p>{topping.price}</p>
-          </dt>
-        </dl>
-      )
-    });
-
-    const drinks = this.state.menu.drinks.map((drink, i) => {
-      return (
-        <dl className="card" key={i}>
-          <dt className="card-header">
-          <h3>{drink.title}</h3>
-          </dt>
-          <dt className="card-body">
-          <p>{drink.price}</p>
-          </dt>
-        </dl>
-      )
-    })
-  }
 }
-
 export default MenuButton
