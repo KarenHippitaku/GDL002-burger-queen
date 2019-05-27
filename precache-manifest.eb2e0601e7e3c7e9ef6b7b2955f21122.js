7ba085f685f01d6a99db0d83687f53965102eec2
KarenHippitaku <hippitaku@gmail.com>
2019-05-27 13:10:16 -0500

    Updates

---
+++ precache-manifest.eb2e0601e7e3c7e9ef6b7b2955f21122.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39b9ead8c1a609b656a09b65b2b496e6",
+    "url": "/gdl002-burger-queen/index.html"
+  },
+  {
+    "revision": "6617d9d9aee3e8cb3568",
+    "url": "/gdl002-burger-queen/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "11a7c8fccb9a5314af48",
+    "url": "/gdl002-burger-queen/static/css/main.1e694901.chunk.css"
+  },
+  {
+    "revision": "6617d9d9aee3e8cb3568",
+    "url": "/gdl002-burger-queen/static/js/2.5939ea79.chunk.js"
+  },
+  {
+    "revision": "11a7c8fccb9a5314af48",
+    "url": "/gdl002-burger-queen/static/js/main.4ebc868d.chunk.js"
+  },
+  {
+    "revision": "abf1e5c4314e6441e9d2",
+    "url": "/gdl002-burger-queen/static/js/runtime~main.e107886b.js"
+  },
+  {
+    "revision": "cfb33a0097e24a796d97a95b1966f38b",
+    "url": "/gdl002-burger-queen/static/media/fondo.cfb33a00.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gdl002-burger-queen/static/media/logo.5d5d9eef.svg"
+  }
+]);
