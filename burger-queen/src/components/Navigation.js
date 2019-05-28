a44266da0570f5e05de22c61aaa96c85259cb4d2
KarenHippitaku <hippitaku@gmail.com>
2019-05-27 23:04:11 -0500

    Independent navigation style created

--- burger-queen/src/components/Navigation.js
+++ burger-queen/src/components/Navigation.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import './Navigation.css';
 // import Dishes from './Dishes';
 // import Drinks from './Drinks';
 // import Toppings from './Toppings';
@@ -9,13 +10,13 @@ class Navigation extends Component {
     return (
       <nav className="navbar navbar-dark bg-dark">
       <img src={logo} className="App-logo" alt="Pomelo-logo.png" />
-      <a href="Dishes" className="text-white">
+      <a href="Dishes" className="text">
       {"Platillos".toUpperCase()}
       </a>
-      <a href="Toppings"className="text-white">
+      <a href="Toppings" className="text">
       {"Toppings".toUpperCase()}
       </a>
-      <a href="Drinks" className="text-white">
+      <a href="Drinks" className="text">
       {"Bebidas".toUpperCase()}
       </a>
       </nav>
